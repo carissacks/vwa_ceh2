be3fdb8388ddc1557b88b9f55d9f72e9ee9fc049
carissacks <carissa.cks@gmail.com>
2020-03-16 19:12:34 +0700

    review ok

--- addReview.php
+++ addReview.php
@@ -16,13 +16,15 @@
 
             $query = "SELECT idUser FROM user WHERE username = '$username' LIMIT 1";
             $result = $conn->query($query);
+            var_dump($result);
 
             if($result->num_rows > 0){
                 while($row = $result->fetch_assoc()){
                     $idUser = $row['idUser'];
                 }
-                $query = "INSERT INTO comment(idUser, idProduct, comment) VALUES($idUser, $idProduct, $message)";
+                $query = "INSERT INTO comment(idUser, idProduct, comment) VALUES($idUser, $idProduct, '$message')";
                 $result = $conn->query($query);
+                echo "berhasil";
             }
             echo "A";
             header("location:product_detail.php?idProduct=".$idProduct);            
