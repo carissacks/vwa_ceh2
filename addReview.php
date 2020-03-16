c350bd1829ab401838cb3e5701b9abc03d3d4519
ChristiantoCunnyen <55533004+ChristiantoCunnyen@users.noreply.github.com>
2020-03-16 18:28:46 +0700

    ProductDetail + addReview

---
+++ addReview.php
@@ -0,0 +1,24 @@
+<?php
+    include 'connect_db.php';
+
+    if(isset($_COOKIE['SID'])){
+        if(isset($_POST['submitReview'])){
+            $idProduct = $_POST['idProduct'];
+            $message = $_POST['reviewMessage'];
+            $cookie = $_COOKIE['SID'];
+            $arrayCookie = explode("#",base64_decode(base64_decode($cookie)));
+            $username = $arrayCookie[1];
+
+            $query = "SELECT idUser FROM user WHERE username = $username LIMIT 1";
+            $result = $conn->query($query);
+
+            if($result->num_rows > 0){
+                while($row = $result->fetch_assoc()){
+                    $idUser = $row['idUser'];
+                }
+                $query = "INSERT INTO comment(idUser, idProduct, comment) VALUES($idUser, $idProduct, $message)";
+                $result = $conn->query($query);
+            }            
+        }
+    }
+?>
--- product_detail.php
+++ product_detail.php
@@ -17,9 +17,94 @@
 	<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
 	<link rel="stylesheet" type="text/css" href="styles/single_responsive.css">
 </head>
+<style>
+  /* The Modal (background) */
+  .modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    padding-top: 100px; /* Location of the box */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+  }
+
+  /* Modal Content */
+  .modal-content {
+    position: relative;
+    background-color: #fefefe;
+    margin: auto;
+    padding: 0;
+    border: 1px solid #888;
+    width: 50%;
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
+    -webkit-animation-name: animatetop;
+    -webkit-animation-duration: 0.4s;
+    animation-name: animatetop;
+    animation-duration: 0.4s
+  }
+
+  /* Add Animation */
+  @-webkit-keyframes animatetop {
+    from {top:-300px; opacity:0} 
+    to {top:0; opacity:1}
+  }
+
+  @keyframes animatetop {
+    from {top:-300px; opacity:0}
+    to {top:0; opacity:1}
+  }
+
+  .close {
+    color: white;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+  }
+
+  .close:hover,
+  .close:focus {
+    color: #000;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  .modal-header {
+    padding: 2px 16px;
+    background-color: #FE4C50;
+    color: white;
+  }
+</style>
 
 <body>
+  	<?php
+		include 'connect_db.php';
+		  
+		$idProduct = $_GET['idProduct'];
+		  
+		$query = "SELECT product_name, product_desc, product_image, product_price FROM product WHERE idProduct = $idProduct LIMIT 1";
+		$result = $conn->query($query);
 
+		if($result->num_rows > 0){
+			while($row = $result->fetch_assoc()){
+				$productName = $row['product_name'];
+				$productDesc = $row['product_desc'];
+				$productImage = $row['product_image'];
+				$productPrice = $row['produce_price'];
+			}
+		}
+		else{
+			$productName = "No Product With Such Id";
+			$productDesc = "No Product With Such Id";
+			$productImage = "";
+			$productPrice = "-";
+		}
+		
+	?>
 	<div class="super_container">
 
 		<!-- Header -->
@@ -95,22 +180,10 @@
 				<div class="col-lg-7">
 					<div class="single_product_pics">
 						<div class="row">
-							<div class="col-lg-3 thumbnails_col order-lg-1 order-2">
-								<div class="single_product_thumbnails">
-									<ul>
-										<li><img src="images/single_1_thumb.jpg" alt=""
-												data-image="images/single_1.jpg"></li>
-										<li class="active"><img src="images/single_2_thumb.jpg" alt=""
-												data-image="images/single_2.jpg"></li>
-										<li><img src="images/single_3_thumb.jpg" alt=""
-												data-image="images/single_3.jpg"></li>
-									</ul>
-								</div>
-							</div>
-							<div class="col-lg-9 image_col order-lg-2 order-1">
+							<div class="col-lg-12 image_col order-lg-2 order-1">
 								<div class="single_product_image">
 									<div class="single_product_image_background"
-										style="background-image:url(images/single_2.jpg)"></div>
+										style="background-image:url(images/<?php echo $productImage; ?>)"></div>
 								</div>
 							</div>
 						</div>
@@ -119,14 +192,13 @@
 				<div class="col-lg-5">
 					<div class="product_details">
 						<div class="product_details_title">
-							<h2>Pocket cotton sweatshirt</h2>
-							<p>Nam tempus turpis at metus scelerisque placerat nulla deumantos solicitud felis.
-								Pellentesque diam dolor, elementum etos lobortis des mollis ut...</p>
+							<h2><?php echo $productName; ?></h2>
+							<p><?php echo $productDesc; ?></p>
 						</div>
 						<div class="free_delivery d-flex flex-row align-items-center justify-content-center">
 							<span class="ti-truck"></span><span>free delivery</span>
 						</div>
-						<div class="product_price mt-4">$495.00</div>
+						<div class="product_price mt-4">$<?php echo $productPrice; ?></div>
 						<ul class="star_rating">
 							<li><i class="fa fa-star" aria-hidden="true"></i></li>
 							<li><i class="fa fa-star" aria-hidden="true"></i></li>
@@ -135,7 +207,7 @@
 							<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
 						</ul>
 						<div class="quantity d-flex flex-column flex-sm-row align-items-sm-center">
-							<div class="red_button add_to_cart_button"><a href="#">add to cart</a></div>
+							<div id="buyItem" class="red_button add_to_cart_button" style="color: white">Buy Item</div>
 						</div>
 					</div>
 				</div>
@@ -203,18 +275,6 @@
 									<!-- User Review -->
 
 									<div class="user_review_container d-flex flex-column flex-sm-row">
-										<div class="user">
-											<div class="user_pic"></div>
-											<div class="user_rating">
-												<ul class="star_rating">
-													<li><i class="fa fa-star" aria-hidden="true"></i></li>
-													<li><i class="fa fa-star" aria-hidden="true"></i></li>
-													<li><i class="fa fa-star" aria-hidden="true"></i></li>
-													<li><i class="fa fa-star" aria-hidden="true"></i></li>
-													<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
-												</ul>
-											</div>
-										</div>
 										<div class="review">
 											<div class="review_date">27 Aug 2016</div>
 											<div class="user_name">Brandon William</div>
@@ -226,18 +286,28 @@
 									<!-- User Review -->
 
 									<div class="user_review_container d-flex flex-column flex-sm-row">
-										<div class="user">
-											<div class="user_pic"></div>
-											<div class="user_rating">
-												<ul class="star_rating">
-													<li><i class="fa fa-star" aria-hidden="true"></i></li>
-													<li><i class="fa fa-star" aria-hidden="true"></i></li>
-													<li><i class="fa fa-star" aria-hidden="true"></i></li>
-													<li><i class="fa fa-star" aria-hidden="true"></i></li>
-													<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
-												</ul>
-											</div>
+										<div class="review">
+											<div class="review_date">27 Aug 2016</div>
+											<div class="user_name">Brandon William</div>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+												tempor incididunt ut labore et dolore magna aliqua.</p>
 										</div>
+									</div>
+
+									<!-- User Review -->
+
+									<div class="user_review_container d-flex flex-column flex-sm-row">
+										<div class="review">
+											<div class="review_date">27 Aug 2016</div>
+											<div class="user_name">Brandon William</div>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+												tempor incididunt ut labore et dolore magna aliqua.</p>
+										</div>
+									</div>
+
+									<!-- User Review -->
+
+									<div class="user_review_container d-flex flex-column flex-sm-row">
 										<div class="review">
 											<div class="review_date">27 Aug 2016</div>
 											<div class="user_name">Brandon William</div>
@@ -252,33 +322,20 @@
 								<div class="col-10 col-md-4 add_review_col">
 
 									<div class="add_review">
-										<form id="review_form" action="post">
+										<form id="review_form" action="addReview.php" method="post">
+  											<input type="text" name="idProduct" value="<?php echo $idProduct;?>" hidden>
 											<div>
 												<h1>Add Review</h1>
-												<input id="review_name" class="form_input input_name" type="text"
-													name="name" placeholder="Name*" required="required"
-													data-error="Name is required.">
-												<input id="review_email" class="form_input input_email" type="email"
-													name="email" placeholder="Email*" required="required"
-													data-error="Valid email is required.">
 											</div>
 											<div>
-												<h1>Your Rating:</h1>
-												<ul class="user_star_rating">
-													<li><i class="fa fa-star" aria-hidden="true"></i></li>
-													<li><i class="fa fa-star" aria-hidden="true"></i></li>
-													<li><i class="fa fa-star" aria-hidden="true"></i></li>
-													<li><i class="fa fa-star" aria-hidden="true"></i></li>
-													<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
-												</ul>
-												<textarea id="review_message" class="input_review" name="message"
+												<textarea id="review_message" class="input_review" name="reviewerMessage"
 													placeholder="Your Review" rows="4" required
 													data-error="Please, leave us a review."></textarea>
 											</div>
 											<div class="text-left text-sm-right">
-												<button id="review_submit" type="submit"
+												<button id="review_submit" type="submit" name="submitReview"
 													class="red_button review_submit_btn trans_300"
-													value="Submit">submit</button>
+													value="Submit">Submit</button>
 											</div>
 										</form>
 									</div>
@@ -379,6 +436,47 @@
 			</div>
 		</footer>
 
+		<div id="myModal" class="modal">
+
+		<div class="container-fluid">
+			<div class="modal-content">
+				<div class="modal-header" style="padding: 2%">
+					<h2 style="color:white">Buy This Product</h2>
+					<span class="close">&times;</span>
+				</div>
+				<div class="modal-body">
+					<form class="form" action="addProduct.php" method="post">
+					<div class="form-group col-8">
+						<b for="Payment Type">Payment Type</b><br>
+						<div class="radio">
+							<input id="optionCardNumber" type="radio" name="radioOption" style="transform: scale(1.3)"><label style="margin-left: 2%">Credit Card Number</label>
+						</div>
+						<div class="radio">
+							<input id="optionUserBalance" type="radio" name="radioOption" style="transform: scale(1.3)"><label style="margin-left: 2%">User Balance</label>
+						</div>
+					</div>
+					<div class="form-group col-8">
+						<div id="formCardNumber">
+							<b for="User Address">Input Credit Card Number</b><br>
+							<input id="cardNumber" class="input form-control" type="text" name="cardNumber" required>
+						</div>
+						<div id="formUserBalance">
+							<b for="User Address">Your User Balance</b><br>
+							<input id="userBalance" class="input form-control" type="text" name="cardNumber" value="1" disabled>
+						</div>
+					</div>
+					<div class="form-group col-8">
+						<b for="User Address">Shipping Address</b><br>
+						<input id="address" class="input form-control" type="text" name="userAddress" required>
+					</div>
+					<div class="form-group col-12 text-right">
+						<input class="btn btn-success" type="submit" name="addTransaction" value="Buy Product">
+					</div>
+				</form>
+				</div>``
+			</div>
+		</div>
+
 	</div>
 
 	<script src="js/jquery-3.2.1.min.js"></script>
@@ -389,6 +487,39 @@
 	<script src="plugins/easing/easing.js"></script>
 	<script src="plugins/jquery-ui-1.12.1.custom/jquery-ui.js"></script>
 	<script src="js/single_custom.js"></script>
+	<script>
+	$(document).ready(function(){
+		var modal = document.getElementById("myModal");
+		var btn = document.getElementById("buyItem");
+		var span = document.getElementsByClassName("close")[0];
+
+		$('#formCardNumber').hide();
+		$('#formUserBalance').hide();
+
+		btn.onclick = function() {
+			modal.style.display = "block";
+		}
+		span.onclick = function() {
+			modal.style.display = "none";
+		}
+		window.onclick = function(event) {
+			if (event.target == modal) {
+				modal.style.display = "none";
+			}
+		}
+
+		$('#optionCardNumber').click(function() {
+		$('#formCardNumber').show();
+		$('#formUserBalance').hide();
+		});
+
+		$('#optionUserBalance').click(function() {
+		$('#formCardNumber').hide();
+		$('#formUserBalance').show();
+		});
+	});
+	</script>
+	
 </body>
 
 </html>
