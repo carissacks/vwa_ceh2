e7f483c8a6929a8b2a73be40d73e861cf71acad3
carissacks <carissa.cks@gmail.com>
2020-03-19 14:57:23 +0700

    title

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Colo Shop</title>
+    <title>VWA CEH2</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="Colo Shop Template">
